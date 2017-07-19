3901468f496eabf0fa277d05cea7a6c1ca3c0d1e
Thomas Mulc <tmulc@expedia.com>
2017-07-19 17:48:52 +0900

    mult gpu single machine

--- DistributedComputing/dist_mult_gpu_sing_mach.py
+++ DistributedComputing/dist_mult_gpu_sing_mach.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 import tensorflow as tf
 import argparse
 import time
-import os
 FLAGS = None
 log_dir = '/logdir'
 
@@ -17,7 +16,7 @@ def main():
 		is_chief = (FLAGS.task_index == 0) #checks if this is the chief node
 		server = tf.train.Server(cluster,job_name="worker",task_index=FLAGS.task_index)
 		# Graph
-		with tf.device('/cpu:0'):
+		with tf.device('/gpu:0'):
 			a = tf.Variable(tf.truncated_normal(shape=[2]),dtype=tf.float32)
 			b = tf.Variable(tf.truncated_normal(shape=[2]),dtype=tf.float32)
 			c=a+b
@@ -28,10 +27,11 @@ def main():
 			opt = tf.train.GradientDescentOptimizer(.0001).minimize(loss)
 
 		# Session
-		sv = tf.train.Supervisor(logdir=os.getcwd()+log_dir,is_chief=is_chief,save_model_secs=30)
-		sess = sv.prepare_or_wait_for_session(server.target)
+		sv = tf.train.Supervisor(logdir=log_dir)
+		gpu_options = tf.GPUOptions(allow_growth=True,allocator_type="BFC",visible_device_list="%d"%FLAGS.task_index)
+		config = tf.ConfigProto(gpu_options=gpu_options,allow_soft_placement=False,device_count={'GPU':1},log_device_placement=True)
+		sess = sv.prepare_or_wait_for_session(config=config)
 		for i in range(1000):
-			if sv.should_stop(): break
 			sess.run(opt)
 			if i % 10 == 0:
 				r = sess.run(c)
@@ -40,19 +40,11 @@ def main():
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	# Flags for defining the tf.train.ClusterSpec
 	parser.add_argument(
-    	"--job_name",
-    	type=str,
-    	default="",
-    	help="One of 'ps', 'worker'"
-    )
-    # Flags for defining the tf.train.Server
-	parser.add_argument(
-    	"--task_index",
-    	type=int,
-    	default=0,
-    	help="Index of task within the job"
-    )
+		"--gpu_id",
+		type=int,
+		default=0,
+		help="ID of GPU to allocate'"
+	)
 	FLAGS, unparsed = parser.parse_known_args()
 	main()
---
+++ DistributedComputing/dist_mult_gpu_sing_mach.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source activate tensorflow1
+python dist_mult_gpu_sing_mach.py --job_name "ps" --task_index 0 &
+python dist_mult_gpu_sing_mach.py --job_name "ps" --task_index 1 &
+python dist_mult_gpu_sing_mach.py --job_name "worker" --task_index 0 &
+python dist_mult_gpu_sing_mach.py --job_name "worker" --task_index 1 &
