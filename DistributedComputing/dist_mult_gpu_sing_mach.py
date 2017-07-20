2eed0ce9f57679c2ebfe38651afd73dcda71dc5f
Floptical <tmulc18@gmail.com>
2017-07-20 01:10:58 -0500

    multiple gpu code running efficiently

--- DistributedComputing/dist_mult_gpu_sing_mach.py
+++ DistributedComputing/dist_mult_gpu_sing_mach.py
@@ -8,21 +8,20 @@ log_dir = '/logdir'
 
 def main():
 	#Distributed Baggage
-	with tf.device('/cpu:0'):
-                cluster = tf.train.ClusterSpec({'ps':['localhost:2222','localhost:2221'],
-                                                'worker':['localhost:2223','localhost:2224']}) #lets this node know about all other nodes
+	#with tf.device('/cpu:0'):
+        cluster = tf.train.ClusterSpec({'ps':['localhost:2222'],
+                                        'worker':['localhost:2223','localhost:2224']}) #lets this node know about all other nodes
 	if FLAGS.job_name == 'ps': #checks if parameter server
 		#gpu_options = tf.GPUOptions(allow_growth=True,allocator_type="BFC",visible_device_list="%d"%FLAGS.task_index)
-                
-		gpu_options = tf.GPUOptions(allow_growth=True,allocator_type="BFC",visible_device_list="")
-		config = tf.ConfigProto(allow_soft_placement=False,device_count={'GPU':0},gpu_options=gpu_options)
+		#gpu_options = tf.GPUOptions(allow_growth=True,allocator_type="BFC",visible_device_list="0")
+		#config = tf.ConfigProto(allow_soft_placement=True,device_count={'GPU':1},log_device_placement=True,gpu_options=gpu_options)
 		with tf.device('/cpu:0'):
-			server = tf.train.Server(cluster,job_name="ps",task_index=FLAGS.task_index,config=config)
+			server = tf.train.Server(cluster,job_name="ps",task_index=FLAGS.task_index)
 			server.join()
 	else:
 		is_chief = (FLAGS.task_index == 0) #checks if this is the chief node
-		#gpu_options = tf.GPUOptions(allow_growth=True,allocator_type="BFC",visible_device_list="%d"%FLAGS.task_index)
-                config = tf.ConfigProto(allow_soft_placement=False,device_count={'GPU':0})
+		gpu_options = tf.GPUOptions(allow_growth=True,allocator_type="BFC",visible_device_list="")
+                config = tf.ConfigProto(allow_soft_placement=True,device_count={'GPU':0})
 		with tf.device('/cpu:0'):	
 			server = tf.train.Server(cluster,job_name="worker",
 						task_index=FLAGS.task_index,config=config)
@@ -40,7 +39,7 @@ def main():
 		# Session
 		sv = tf.train.Supervisor(logdir=os.getcwd()+log_dir,is_chief=is_chief,save_model_secs=30)
 		gpu_options = tf.GPUOptions(allow_growth=True,allocator_type="BFC",visible_device_list="%d"%FLAGS.task_index)
-		config = tf.ConfigProto(gpu_options=gpu_options,allow_soft_placement=True,device_count={'GPU':1})
+		config = tf.ConfigProto(gpu_options=gpu_options,allow_soft_placement=True)#,device_count={'GPU':1})
 		sess = sv.prepare_or_wait_for_session(server.target,config=config)
 		for i in range(1000):
 			if sv.should_stop(): break
