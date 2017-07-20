297dcd8c08bf6749daad694e2d6d486a732cd56c
Floptical <tmulc18@gmail.com>
2017-07-19 22:36:52 -0500

    mult gpu working but not optimal

--- DistributedComputing/dist_mult_gpu_sing_mach.sh
+++ DistributedComputing/dist_mult_gpu_sing_mach.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-source activate tensorflow1
+#source activate tensorflow1n
 python dist_mult_gpu_sing_mach.py --job_name "ps" --task_index 0 &
 python dist_mult_gpu_sing_mach.py --job_name "ps" --task_index 1 &
+#source activate tensorflow1
 python dist_mult_gpu_sing_mach.py --job_name "worker" --task_index 0 &
 python dist_mult_gpu_sing_mach.py --job_name "worker" --task_index 1 &
